c041f1ed0ce0057e95285d29a961c855b1e39312
Sebastian Velandia <sebas.velandia27@gmail.com>
2014-11-14 22:22:09 -0500

    Add length and presence validation to todo list title

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,2 +1,4 @@
 class TodoList < ActiveRecord::Base
+	validates :title, presence: true
+	validates :title, length: { minimum: 3 }
 end
---
+++ spec/features/todo_lists/create_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe "Creating todo lists" do
+	it "redirects to the tod list index page on success" do
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: "My todo list"
+		fill_in "Description", with: "This is what I'm doing today."
+		click_button "Create Todo list"
+
+		expect(page).to have_content("My todo list")
+	end
+
+	it "displays an error when the todo list has no title" do
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: ""
+		fill_in "Description", with: "This is what I'm doing today."
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("This is what I'm doing today.")
+	end
+
+	it "displays an error when the todo list has a title less than 2 characters" do
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: "Hi"
+		fill_in "Description", with: "This is what I'm doing today."
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("This is what I'm doing today.")
+	end
+
+end
