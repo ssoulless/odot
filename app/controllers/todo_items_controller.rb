7d3bd6ae4b17efed525da5b36a18cace64e80328
Sebastian Velandia <sebas.velandia27@gmail.com>
2014-11-15 14:41:39 -0500

    Write tests for todo items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,4 +1,5 @@
 class TodoItemsController < ApplicationController
   def index
+  	@todo_list = TodoList.find(params[:todo_list_id])
   end
 end
--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -2,11 +2,37 @@ require 'spec_helper'
 
 describe "Viewing todo items" do
 	let!(:todo_list) { TodoList.create(title: "Grocery list", description: "Grocery list description") } 
-	it "displays no items when a todo list is empty" do
+
+	def visit_todo_list list
 		visit "/todo_lists"
-		within "#todo_list_#{todo_list.id}" do
+		within "#todo_list_#{list.id}" do
 			click_link "List Items"
 		end
-		expect(page).to have_content(%Q|TodoItems#index|)
 	end
+
+	it "displays the title of the todo list" do
+		visit_todo_list(todo_list)
+		within("h1") do
+			expect(page).to have_content(todo_list.title)
+		end	
+	end
+
+	it "displays no items when a todo list is empty" do
+		visit_todo_list(todo_list)
+		expect(page.all("ul.todo_items li").size).to eq(0)
+	end
+
+	it "displays item content when a todo list has items" do
+		todo_list.todo_items.create(content: %Q|Milk|)	
+		todo_list.todo_items.create(content: %Q|Eggs|)	
+
+		visit_todo_list(todo_list)
+
+		expect(page.all("ul.todo_items li").size).to eq(2)
+		within "ul.todo_items" do
+			expect(page).to have_content(%Q|Milk|)
+			expect(page).to have_content(%Q|Eggs|)
+		end
+	end
+
 end
