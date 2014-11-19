acc86b1a0e69d7ce48ed25a714eb68ba050748a9
Sebastian Velandia <sebas.velandia27@gmail.com>
2014-11-19 16:00:33 -0500

    Cleaning up the code of the items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,15 +1,15 @@
 class TodoItemsController < ApplicationController
+  before_action :find_todo_list
+
   def index
-  	@todo_list = TodoList.find(params[:todo_list_id])
+
   end
 
   def new
-  	@todo_list = TodoList.find(params[:todo_list_id])
   	@todo_item = @todo_list.todo_items.new
   end
 
   def create
-  	@todo_list = TodoList.find(params[:todo_list_id])
   	@todo_item = @todo_list.todo_items.new(todo_item_params)
   	if @todo_item.save
   		flash[:success] = %Q|Added todo list item.|
@@ -21,13 +21,10 @@ class TodoItemsController < ApplicationController
   end
 
   def edit
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.find(params[:id])
-
   end
 
   def update
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.update_attributes(todo_item_params)
       flash[:success] = %Q|Saved todo list item.|
@@ -43,6 +40,10 @@ class TodoItemsController < ApplicationController
   end
 
   private
+  def find_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
   def todo_item_params
   	params[:todo_item].permit(:content)
   end
