9a778cf1bd2cea3b40cb64b9bc6c92b224e33e0b
Sebastian Velandia <sebas.velandia27@gmail.com>
2015-01-15 11:45:19 -0500

    Added factory girl support to the application, also added support for the new AuthenticationHelper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,6 +21,8 @@ RSpec.configure do |config|
   config.include TodoListHelpers, type: :feature
   config.include RailsDomIdHelper, type: :feature
   config.expose_current_running_example_as :example
+  config.include FactoryGirl::Syntax::Methods
+  config.include AuthenticationHelpers, type: :controller
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
