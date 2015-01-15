d49c56fb39a422af347ae0a572db15554a050664
Sebastian Velandia <sebas.velandia27@gmail.com>
2015-01-15 14:10:48 -0500

    Added /login and /logout paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  get "/login" => "user_sessions#new", as: :login
+  delete "/logout" => "user_sessions#destroy", as: :logout
+
   resources :users
 
   resources :user_sessions, only: [:new, :create]
