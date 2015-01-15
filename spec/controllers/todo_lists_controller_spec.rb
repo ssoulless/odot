2d4dfd2f4457168f2650497e14d899b390fbc4a0
Sebastian Velandia <sebas.velandia27@gmail.com>
2015-01-15 17:12:19 -0500

    cleaned up the code of todo_lists_controller_spec.rb

--- spec/controllers/todo_lists_controller_spec.rb
+++ spec/controllers/todo_lists_controller_spec.rb
@@ -28,11 +28,12 @@ describe TodoListsController do
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # TodoListsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  let(:valid_session) { {} }   
+  let!(:user) { create(:user) }
 
   before do
     #allow(controller).to receive(:require_user).and_return(true)
-    sign_in(create(:user))
+    sign_in(user)
   end
 
   describe "GET index" do
