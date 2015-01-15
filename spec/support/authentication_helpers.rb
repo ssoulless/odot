d199069e7a25855073521245ff2a30efa7464e2b
Sebastian Velandia <sebas.velandia27@gmail.com>
2015-01-15 14:05:21 -0500

    coped authentication_helpers.rb to work with controllers and features

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -1,6 +1,17 @@
 module AuthenticationHelpers
-	def sign_in(user)
-		controller.stub(:require_user).and_return(true)
-#		controller.stub(:user_id).and_return(user.id)
+	module Controller
+		def sign_in(user)
+			controller.stub(:require_user).and_return(true)
+	#		controller.stub(:user_id).and_return(user.id)
+		end
+	end
+
+	module Feature
+		def sign_in(user, options={})
+			visit "/login"
+			fill_in "Email", with: user.email
+			fill_in "Password", with: options[:password]
+			click_button "Log In"
+		end
 	end
 end
