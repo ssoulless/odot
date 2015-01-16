4f3d3e2a3379e2a467b36d270ca70794b5da99c8
Sebastian Velandia <sebas.velandia27@gmail.com>
2015-01-15 19:08:41 -0500

    Added scope of current_user to todo_items controller

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -58,7 +58,7 @@ class TodoItemsController < ApplicationController
 
   private
   def find_todo_list
-    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_list = current_user.todo_lists.find(params[:todo_list_id])
   end
 
   def todo_item_params
