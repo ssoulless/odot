780b9adf793b92df8b9b96dc7a72ed1a6a34ae4c
Sebastian Velandia <sebas.velandia27@gmail.com>
2015-01-17 17:59:47 -0500

    Added validatoin to password_resets_controller.rb when user is not found when requesting new password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,8 +5,15 @@ class PasswordResetsController < ApplicationController
 
 	def create
 		user = User.find_by(email: params[:email])
-		user.generate_password_reset_token!
-		redirect_to login_path
+		if user
+			user.generate_password_reset_token!
+			Notifier.password_reset(user).deliver
+			flash[:success] = "Further instructions were sent to your email"
+			redirect_to login_path
+		else
+			flash.now[:error] = "There is not an account with that email"
+			render action: 'new'
+		end
 	end
 
 end
