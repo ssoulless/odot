af66a35556994a78142de80fcddbb67754485f6b
Sebastian Velandia <sebas.velandia27@gmail.com>
2015-01-17 18:02:39 -0500

    Added configs for mailer

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,3 +1,10 @@
 class Notifier < ActionMailer::Base
+  default_url_options[:host] = "localhost:3000"
   default from: "from@example.com"
+
+  def password_reset(user)
+  	@user = user
+  	mail(to: "#{user.first_name} #{user.last_name} <#{user.email}>",
+  		 subject: "Reset password")
+  end
 end
