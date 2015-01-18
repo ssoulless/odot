8b1e0ba57c3b107d6f2b11e916765df638105cf6
Sebastian Velandia <sebas.velandia27@gmail.com>
2015-01-18 15:45:57 -0500

    Added conditional that checks if user checked remember me checkbox in the login page for then set the cookie, also added deletion of the cookie once logging out

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,6 +5,10 @@ class UserSessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:email])
   	if user && user.authenticate(params[:password])
+      if params[:remember_me]
+        signed_token = Rails.application.message_verifier(:remember_me).generate(user.id)
+        cookies.permanent.signed[:remember_me_token] = signed_token
+      end
 	  	session[:user_id] = user.id
 	  	flash[:success] = %Q|Thanks for logging in|
 	  	redirect_to todo_lists_path
@@ -16,8 +20,9 @@ class UserSessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    cookies.delete(:remember_me_token)
     reset_session
-    redirect_to root_path, success: "You have been logged out."
+    redirect_to root_path, notice: "You have been logged out."
   end
 end
 	
