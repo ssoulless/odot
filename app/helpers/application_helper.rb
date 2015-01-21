8b994bd52359831e8968be09441b892bf45332ce
Sebastian Velandia <sebas.velandia27@gmail.com>
2015-01-21 15:57:40 -0500

    Added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,13 @@
 module ApplicationHelper
+	def title(title)
+		content_for(:title) { "#{title} | " }
+	end
+
 	def new_item_link
-		#<a href="#" class="icon-new right hide-text">Add Item</a>
 		if @todo_list && !@todo_list.new_record?
-			text = "Todo Item"
-			path = new_todo_list_todo_item_path(@todo_list)
+			text , path = "Todo Item", new_todo_list_todo_item_path(@todo_list)
 		else
-			text = "Todo List"
-			path = new_todo_list_path
+			text, path = "Todo List", new_todo_list_path
 		end
 
 		link_to "Add #{text}", path, class: "icon-new right hide-text"
