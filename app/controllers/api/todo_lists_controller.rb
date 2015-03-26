9b05b73e8828e842dba9a3b68023836f580454ce
Sebastian Velandia <sebas.velandia27@gmail.com>
2015-03-25 22:55:47 -0500

    added more methods and also fixed http code status in order to show the correct one

--- app/controllers/api/todo_lists_controller.rb
+++ app/controllers/api/todo_lists_controller.rb
@@ -10,12 +10,37 @@ class Api::TodoListsController < ApplicationController
 	def create
 		list = TodoList.new(list_params)
 		if list.save
-			head 200
+			render status: 200, json: {
+				message: "Successfully created todo-list.",
+				todo_list: list
+			}.to_json
 		else
-			head 500
+			render status: 422, json: {
+				errors: list.errors
+			}.to_json
 		end
 	end
 
+	def destroy
+		list = TodoList.find(params[:id])
+		render status: 200, json: {
+			message: "Successfully deleted todo_list",
+		}.to_json
+	end
+
+	def update
+		list = TodoList.find(params[:id])
+		if list.update(list_params)
+			render status: 200, json: {
+				message: "Successfully updated todo-list.",
+				todo_list: list
+			}.to_json
+		else
+			render status: 422, json: {
+				errors: list.errors
+			}.to_json
+		end
+	end
 	private
 	def list_params
 		params.require("todo_list").permit("title")
