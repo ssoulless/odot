557473c7af9c38d7dbb1c7d2d0905973f43644b9
Sebastian Velandia <sebas.velandia27@gmail.com>
2015-03-25 23:14:46 -0500

    Added routes for nested todo items in todo lists for API namespace, also created a new controller for handle API actions for todo_items, added create action in Api::TodoItemsController

---
+++ app/controllers/api/todo_items_controller.rb
@@ -0,0 +1,27 @@
+class Api::TodoItemsController < ApplicationController
+	skip_before_filter :verify_authenticity_token
+	before_filter :find_todo_list
+
+	def create
+		item = @todo_list.todo_items.new(item_params)
+		if item.save
+			render status: 200, json: {
+				message: "Successfully created todo item.",
+				todo_item: item
+			}.to_json
+		else
+			render status: 422, json: {
+				message: "Todo Item creation failed",
+				errors: item.errors,
+			}.to_json
+		end
+	end
+
+	private
+	def item_params
+		params.require("todo_item").permit("content")
+	end
+	def find_todo_list
+		@todo_list = TodoList.find(params[:todo_list_id])
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :todo_lists
+    resources :todo_lists do 
+      resources :todo_items, only: [:create, :update, :destroy]
+    end
   end
 
   get 'pages/home'
